46d61d9eee419edc7ea710ca7f477068ecdbde1f
drkanukie <admin@fullthrust.net>
2022-04-08 14:25:50 +0100

    New repo

---
+++ .eleventy.js
@@ -0,0 +1,9 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('styles');
+  eleventyConfig.addPassthroughCopy('js');
+  eleventyConfig.addPassthroughCopy('assets');
+  return {
+    passthroughFileCopy: true
+  }
+};
+
---
+++ index.md
@@ -0,0 +1,12 @@
+---
+layout: layout.liquid
+title: Morena Shipyards
+---
+# Welcome to Morena Shipyards
+
+This site is designed to host the content for the Morena Shipyards mailing list.
+
+## Projects
+
+## Links
+
